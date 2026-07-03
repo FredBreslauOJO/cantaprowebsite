1c5ed7da972ab64d16f101838ffea477b209b820
Fred Breslau <fred@ojo-studio.com>
2026-07-03 19:51:19 -0300

    next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Libera acessos locais que configuramos antes
+  // Libera acessos locais que configuramos  antes
   allowedDevOrigins: ['192.168.2.199', 'localhost:3000'],
   
   // IGNORA erros chatos de formatação e tipagem na hora de subir pra Vercel
